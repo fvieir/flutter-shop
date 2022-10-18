258834a501cd50cf6e8a59367bfb34a4ceb27d8e
fvieir <fcvieira2@gmail.com>
2022-10-18 12:57:28 -0300

    Add navvegação para pagina do carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
         home: const ProductOverviewPage(),
         routes: {
           AppRoutes.productDetails: (context) => const ProductDetailPage(),
+          AppRoutes.cart: (context) => const CartPage(),
         },
       ),
     );
