64ba7f8065582d80f4f824759428bb201e0ea3b6
fvieir <fcvieira2@gmail.com>
2022-10-20 15:22:52 -0300

    Submetendo formulario

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -12,11 +16,13 @@ class _ProductFormPageState extends State<ProductFormPage> {
   final _descriptionFocus = FocusNode();
   final _imageUrlFocus = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _formData = <String, Object>{};
 
   @override
   void initState() {
     super.initState();
-    _imageUrlController.addListener(updateImage);
+    _imageUrlFocus.addListener(_updateImage);
   }
 
   @override
@@ -26,11 +32,22 @@ class _ProductFormPageState extends State<ProductFormPage> {
     _descriptionFocus.dispose();
 
     _imageUrlFocus.dispose();
-    _imageUrlController.removeListener(updateImage);
+    _imageUrlFocus.removeListener(_updateImage);
   }
 
-  void updateImage() {
+  void _updateImage() {
     setState(() {});
+  }
+
+  void _submitForm() {
+    _formKey.currentState?.save();
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
   }
 
   @override
@@ -38,24 +55,30 @@ class _ProductFormPageState extends State<ProductFormPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulario de Produtos'),
+        actions: [
+          IconButton(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('Nome'),
-                ),
+                decoration: const InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
+                onSaved: (name) => _formData['name'] = name as String,
               ),
               TextFormField(
                 decoration: const InputDecoration(
-                  label: Text('Preço'),
+                  labelText: 'Preço',
                 ),
                 keyboardType:
                     const TextInputType.numberWithOptions(decimal: true),
@@ -64,27 +87,31 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocus);
                 },
+                onSaved: (price) =>
+                    _formData['price'] = double.parse(price ?? '0'),
               ),
               TextFormField(
-                decoration: const InputDecoration(label: Text('Descrição')),
+                decoration: const InputDecoration(labelText: 'Descrição'),
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
                 focusNode: _descriptionFocus,
-                // onFieldSubmitted: (_) =>
-                //     FocusScope.of(context).requestFocus(_imageUrlFocus),
+                onSaved: (description) =>
+                    _formData['description'] = description as String,
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Imagem Url'),
-                      ),
+                      decoration:
+                          const InputDecoration(labelText: 'Imagem Url'),
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.done,
                       focusNode: _imageUrlFocus,
                       controller: _imageUrlController,
+                      onFieldSubmitted: (_) => _submitForm(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? '',
                     ),
                   ),
                   Container(
