ae271a36864b8e8cb62d47326eb8b181c2956dd8
fvieir <fcvieira2@gmail.com>
2022-10-20 13:29:49 -0300

    Add campo descrição no formulario

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -10,11 +8,60 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _focusPrice = FocusNode();
+  final _focusDescription = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _focusPrice.dispose();
+    _focusDescription.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulario de Produtos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Nome'),
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_focusPrice);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Preço'),
+                ),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                focusNode: _focusPrice,
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_focusDescription);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Descrição'),
+                ),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                focusNode: _focusDescription,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
