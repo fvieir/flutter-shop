32a4732080b5cd8ffe254d159c3867b94cfa8dbf
fvieir <fcvieira2@gmail.com>
2022-10-24 15:29:42 -0300

    Add integração com firebase ao marcar com favorito

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/http_exceptions.dart';
+import '../utils/constants.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -7,6 +12,8 @@ class Product with ChangeNotifier {
   final double price;
   final String imageUrl;
   bool isFavorite;
+
+  final String _baseUrl = Constants.productBaseUrl;
 
   Product({
     required this.id,
@@ -17,8 +24,34 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  Future<void> toggleFavorite() async {
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final response = await http
+        .patch(
+      Uri.parse('$_baseUrl/$id.json'),
+      body: jsonEncode(
+        {
+          "isFavorite": isFavorite,
+        },
+      ),
+    )
+        .catchError(
+      (error) {
+        errorToggleFavorite(isFavorite);
+      },
+    );
+
+    if (response.statusCode >= 400) {
+      errorToggleFavorite(isFavorite);
+    }
+  }
+
+  errorToggleFavorite(isFavoriteReceveid, [statusCode]) {
+    isFavorite = !isFavoriteReceveid;
+    notifyListeners();
+
+    throw HttpExceptions(msg: 'Algo deu errado!', statusCode: statusCode);
   }
 }
