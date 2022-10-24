218afa7af25f25ef4c01c63a9af07331fa970fa3
fvieir <fcvieira2@gmail.com>
2022-10-24 17:17:58 -0300

    Add urlbase para carrinho

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-cod3r-f5b93-default-rtdb.firebaseio.com/product';
+  static const orderBaseUrl =
+      'https://shop-cod3r-f5b93-default-rtdb.firebaseio.com/cart';
 }
