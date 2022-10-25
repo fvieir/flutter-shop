5900a12b6ce83f8128bc4c17687336085b71abe1
fvieir <fcvieira2@gmail.com>
2022-10-25 13:02:51 -0300

    Refatorando statefull para stateless usando FutureBuilder

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -4,40 +4,36 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrderPage extends StatefulWidget {
+class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
 
   @override
-  State<OrderPage> createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
-  bool isLoad = false;
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() => isLoad = true);
-    Provider.of<OrderList>(context, listen: false).loadOrder().then((value) {
-      setState(() => isLoad = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
       ),
-      body: isLoad
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, i) {
-                return OrderWidget(order: orders.items[i]);
-              },
-            ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrder(),
+        builder: ((ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text('Algo deu errado!'),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) {
+                  return OrderWidget(order: orders.items[i]);
+                },
+              ),
+            );
+          }
+        }),
+      ),
       drawer: const AppDrawer(),
     );
   }
